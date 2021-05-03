a98948ec503edd9372fb10fec9c1c13edd03de9a
Alon Ewenson <alonewenson@gmail.com>
2021-05-03 21:03:48 +0300

    changed PIXABAY_URL const string

--- controllers/pixabayImgController.js
+++ controllers/pixabayImgController.js
@@ -1,6 +1,7 @@
 
 const axios = require('axios');
 
+//TODO move key to a config file. maybe remove 'image_type=vector'
 const PIXABAY_URL = 'https://pixabay.com/api/?key=13069670-c291adf8946c43c1ea7802448&image_type=vector&category=food&q=';
 const PIXABAY_QUESTION_MARK_IMG = 'https://cdn.pixabay.com/photo/2017/02/13/01/26/the-question-mark-2061539_960_720.png';
 
