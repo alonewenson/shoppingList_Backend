841bff67652fc0cf482ec4645de4122d8b35b706
Alon Ewenson <alonewenson@gmail.com>
2021-05-03 21:02:16 +0300

    changed PIXABAY_URL const string

--- controllers/pixabayImgController.js
+++ controllers/pixabayImgController.js
@@ -1,12 +1,12 @@
 
 const axios = require('axios');
 
-const PIXABAY_URL = 'https://pixabay.com/api/?key=13069670-c291adf8946c43c1ea7802448&image_type=vector&category=food';
+const PIXABAY_URL = 'https://pixabay.com/api/?key=13069670-c291adf8946c43c1ea7802448&image_type=vector&category=food&q=';
 const PIXABAY_QUESTION_MARK_IMG = 'https://cdn.pixabay.com/photo/2017/02/13/01/26/the-question-mark-2061539_960_720.png';
 
 
 const getImgGallery = async (imgName) =>{
-  const url = PIXABAY_URL + '&q=' + imgName;
+  const url = PIXABAY_URL + imgName;
   const pixabayRes = await axios.get(url);
   let result = []
 
